862c0d948ef0b9531647700fb27954c43d9bbd10
Christiam Hernandez <christiamhernandez@Administrators-MacBook-Pro.local>
2019-10-24 15:13:56 -0400

    Added link to student, employee and parents

--- client/src/elements/homenav.js
+++ client/src/elements/homenav.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 function HomeNav(){ 
     return (
@@ -12,7 +11,7 @@ function HomeNav(){
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Link</a>
@@ -28,17 +27,17 @@ function HomeNav(){
           <a className="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
           </input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
-    );  
+    ); 
 }
-export default HomeNav
+export default HomeNav;
